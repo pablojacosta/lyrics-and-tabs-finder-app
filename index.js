50e7c99b619064207eddf8af831895b921a4d6ab
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-09-22 17:14:52 -0300

    feat: :sparkles: Backend updates.

--- index.js
+++ index.js
@@ -1,39 +1,66 @@
-const express = require('express')
-const cors = require('cors')
-const axios = require('axios')
-const cheerio = require('cheerio')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const app = express();
+app.use(cors());
 
-const app = express()
-app.use(cors())
-app.use(express.static('build'))
+app.get("/", (request, response) => {
+  response.send("<h1>LYRICS AND TABS FINDER - BACKEND :)</h1>");
+});
 
-app.get('/lyrics', (req, res) => {
-  
+app.get("/songs", (req, res) => {
   const options = {
-    method: 'GET',
-    url: req.query.passedUrl,
-    responseType: 'text'
-  }
+    method: "GET",
+    url: "https://genius-song-lyrics1.p.rapidapi.com/search/",
+    params: {
+      q: req.query.newSearch,
+      per_page: "10",
+      page: "1",
+    },
+    headers: {
+      "X-RapidAPI-Key": process.env.API_KEY,
+      "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com",
+    },
+  };
 
   axios
     .request(options)
     .then((response) => {
-      const html = response.data
-      const $ = cheerio.load(html)
-   
-      let lyrics = []
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+app.get("/lyrics", (req, res) => {
+  const options = {
+    method: "GET",
+    url: req.query.passedUrl,
+    responseType: "text",
+  };
+
+  axios
+    .request(options)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      let lyrics = [];
 
       $("div[class*='Lyrics__Container']", html).each(function () {
-        let scrappedLyrics = $(this)
-        scrappedLyrics.find('a').each(function () {
-          $(this).replaceWith($(this).find('span').html())
-        })
-        lyrics.push(scrappedLyrics.html())
-      })
-      res.send(lyrics)
-    }).catch((err) => console.log(err))
-  })
+        let scrappedLyrics = $(this);
+        scrappedLyrics.find("a").each(function () {
+          $(this).replaceWith($(this).find("span").html());
+        });
+        lyrics.push(scrappedLyrics.html());
+      });
+      res.send(lyrics);
+    })
+    .catch((err) => console.log(err));
+});
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
